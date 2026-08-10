07f566c63401c23cf1452a5ce9538d5293f2886d
Faheema <mzfaheema@gmail.com>
2026-08-10 20:38:17 +0530

    Temporarily enable debug

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("9kqds8i)^(#g0=f$i@oo2q(1aoy1wxncj^z4!q+q*wasrz3jcl")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 LOGGING = {
